2060b7be85f8a9b34e3fa2108e80fae87b2770d7
CJKBerg <cjkberg@pm.me>
2025-08-28 16:07:39 +0200

    klar med player

--- main.py
+++ main.py
@@ -9,16 +9,24 @@ def main():
           Screen width: {SCREEN_WIDTH}
           Screen height: {SCREEN_HEIGHT}""")
     pygame.init()
+
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        updatable.update(dt)
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
